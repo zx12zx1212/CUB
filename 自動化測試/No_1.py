4b01b8594c19fb7b2f03d3f12fe3383a2fab2cf1
zx12zx1212 <zx12zx1212@gmail.com>
2023-09-13 19:59:50 +0800

    ver.2

--- 自動化測試/No_1.py
+++ 自動化測試/No_1.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
+driver.set_window_size(400, 800)
 driver.get("https://www.cathaybk.com.tw/cathaybk")
+
 try :
     driver.implicitly_wait(5)
     driver.get_screenshot_as_file("./image/test_1.png")
--- 自動化測試/No_3.py
+++ 自動化測試/No_3.py
@@ -18,11 +18,27 @@ driver.execute_script("arguments[0].setAttribute(arguments[1],arguments[2])", xp
 xpath = driver.find_element(By.XPATH, "/html/body/div[1]/header/div/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div/div[1]")
 driver.execute_script("arguments[0].setAttribute(arguments[1],arguments[2])", xpath, "class", "cubre-o-menuLinkList__item is-L2open")
 
-layer_1 = driver.find_elements(By.CLASS_NAME,"cubre-o-menuLinkList__content")
-layer_1 = layer_1[1].find_elements(By.TAG_NAME,"a")
-print(f'共有 {len(layer_1)} 項！')
+time.sleep(2)
+card = driver.find_element(By.ID, 'lnk_Link')
+card.click()
 
-time.sleep(1)
-driver.get_screenshot_as_file("./image/test_2.png")
+xpath = driver.find_element(By.XPATH, "/html/body/div[1]/main/article/div/div/div/div[1]/div")
+driver.execute_script("arguments[0].setAttribute(arguments[1],arguments[2])", xpath, "class", "cubre-m-anchor__slider swiper swiper-container-initialized swiper-container-horizontal swiper-container-free-mode swiper-container-android is-pageEnd")
+
+driver.execute_script("window.scrollBy(0, 3850);")
+
+anchor = driver.find_elements(By.CLASS_NAME,"cubre-o-anchorBlock")
+div = anchor[5].find_elements(By.CLASS_NAME,"cubre-o-slide__item")
+now_size = 0
+size = 465
+for i in range(len(div)):
+    style = anchor[5].find_element(By.CLASS_NAME, "swiper-wrapper")
+    js = f"transform: translate3d({now_size}px, 0px, 0px);transition-duration: 0ms;"
+    driver.execute_script("arguments[0].setAttribute(arguments[1],arguments[2])", style, "style", js)
+    time.sleep(1)
+    now_size -= size
+    driver.get_screenshot_as_file(f"./image/card/card_{i+1}.png")
+
+time.sleep(3)
 driver.quit()
 
